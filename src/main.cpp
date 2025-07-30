25f943c539673720e406aa07314c3c7379bf300a
ccn <ccn@cuppajoeman.com>
2025-07-30 01:45:44 -0400

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,14 @@
-#include <iostream>
-#include <fstream>
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 
-#include "utility/meta_utils/meta_utils.hpp"
 #include "utility/logger/logger.hpp"
+#include "utility/meta_utils/meta_utils.hpp"
 
 #include "functions/string_invoker/functions.hpp"
 #include "utility/regex_utils/regex_utils.hpp"
 
 ConsoleLogger logger;
-
-// NOTE: aboutodo
-// * was adding a whitelist feature that goes all the way down to mfc
-// * then we can do that only on the functions we want to do from vertex_geom
-// * then we can integrate that into the other project.
 
 int main() {
     std::vector<meta_utils::MetaType> extended_concrete_types = meta_utils::concrete_types;
@@ -23,9 +18,11 @@ int main() {
     meta_utils::MetaType glm_vec2_type("glm::vec2", "[](const std::string &s) { return glm_utils::parse_vec2(s); }",
                                        "[](const glm::vec2 &v) { return vec2_to_string(v); }",
                                        regex_utils::float_tuple);
-    meta_utils::MetaType force_type("Force", "[](const std::string &s) { return string_to_force(s); }",
+    meta_utils::MetaType force_type("draw_info::IndexedVertexPositions",
+                                    "[](const std::string &s) { return string_to_force(s); }",
                                     "[](const Force &f) { return force_to_string(f); }",
                                     regex_utils::tuple_of({regex_utils::float_regex, regex_utils::float_triplet}));
+
     meta_utils::MetaType meta_type_type("meta_utils::MetaType", "", "", "MetaType");
 
     extended_concrete_types.push_back(glm_vec3_type);
@@ -36,9 +33,22 @@ int main() {
     std::unordered_map<std::string, std::function<meta_utils::MetaType(meta_utils::MetaType)>>
         extended_generic_type_to_meta_type_constructor = meta_utils::generic_type_to_metatype_constructor;
 
-    meta_utils::generate_string_invokers_from_source_code("src/functions/functions.hpp", "src/functions/functions.cpp",
-                                                          extended_concrete_types, true, {"int add(int x, int y)"},
-                                                          meta_utils::FilterMode::Whitelist);
+    meta_utils::MetaFunctionSignature ivp_constructor(
+        "IndexedVertexPositions(std::vector<unsigned int> indices, std::vector<glm::vec3> xyz_positions, int id = "
+        "GlobalUIDGenerator::get_id())",
+        meta_utils::create_type_name_to_meta_type_map(extended_concrete_types));
+
+    auto x = meta_utils::generate_string_invoker_for_function(ivp_constructor, extended_concrete_types);
+
+    std::cout << "goob" << std::endl;
+    std::cout << x << std::endl;
+
+    meta_utils::generate_string_invokers_from_source_code(
+        "src/graphics/vertex_geometry/vertex_geometry.hpp", "src/graphics/vertex_geometry/vertex_geometry.cpp",
+        extended_concrete_types, true,
+        {" draw_info::IndexedVertexPositions generate_rectangle_between_2d(const "
+         "glm::vec2 &p1, const glm::vec2 &p2, float thickness) "},
+        meta_utils::FilterMode::Whitelist);
 
     std::cout << "Enter function invocation strings (type 'quit' to exit):\n";
 

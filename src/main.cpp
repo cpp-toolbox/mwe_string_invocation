7ea022a25d62d0b4c824bf186ef8dc109f073bf7
ccn <ccn@cuppajoeman.com>
2025-07-31 13:24:26 -0400

    feat: show usage of string invoker on vert geom

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,12 @@
 #include "utility/logger/logger.hpp"
 #include "utility/meta_utils/meta_utils.hpp"
 
-#include "functions/string_invoker/functions.hpp"
+#include "graphics/vertex_geometry/string_invoker/vertex_geometry.hpp"
 #include "utility/regex_utils/regex_utils.hpp"
+
+// NOTE was working on
+// meta_utils::generate_string_invokers_from_source_code(
+// but the function parsing is totoally wrong..., need to fix that. next
 
 ConsoleLogger logger;
 
@@ -18,16 +22,11 @@ int main() {
     meta_utils::MetaType glm_vec2_type("glm::vec2", "[](const std::string &s) { return glm_utils::parse_vec2(s); }",
                                        "[](const glm::vec2 &v) { return vec2_to_string(v); }",
                                        regex_utils::float_tuple);
-    meta_utils::MetaType force_type("draw_info::IndexedVertexPositions",
-                                    "[](const std::string &s) { return string_to_force(s); }",
-                                    "[](const Force &f) { return force_to_string(f); }",
-                                    regex_utils::tuple_of({regex_utils::float_regex, regex_utils::float_triplet}));
 
     meta_utils::MetaType meta_type_type("meta_utils::MetaType", "", "", "MetaType");
 
     extended_concrete_types.push_back(glm_vec3_type);
     extended_concrete_types.push_back(glm_vec2_type);
-    extended_concrete_types.push_back(force_type);
     extended_concrete_types.push_back(meta_type_type);
 
     std::unordered_map<std::string, std::function<meta_utils::MetaType(meta_utils::MetaType)>>
@@ -36,12 +35,18 @@ int main() {
     meta_utils::MetaFunctionSignature ivp_constructor(
         "IndexedVertexPositions(std::vector<unsigned int> indices, std::vector<glm::vec3> xyz_positions, int id = "
         "GlobalUIDGenerator::get_id())",
-        meta_utils::create_type_name_to_meta_type_map(extended_concrete_types));
+        "", meta_utils::create_type_name_to_meta_type_map(extended_concrete_types));
 
     auto x = meta_utils::generate_string_invoker_for_function(ivp_constructor, extended_concrete_types);
 
-    std::cout << "goob" << std::endl;
-    std::cout << x << std::endl;
+    meta_utils::MetaFunction mfx(x, meta_utils::create_type_name_to_meta_type_map(extended_concrete_types));
+
+    meta_utils::MetaType ivp_type(
+        "draw_info::IndexedVertexPositions", mfx.to_lambda_string(),
+        "[](const draw_info::IndexedVertexPositions &ivp) { std::ostringstream oss; oss << ivp; return oss.str(); } ",
+        regex_utils::tuple_of({regex_utils::float_regex, regex_utils::float_triplet}));
+
+    extended_concrete_types.push_back(ivp_type);
 
     meta_utils::generate_string_invokers_from_source_code(
         "src/graphics/vertex_geometry/vertex_geometry.hpp", "src/graphics/vertex_geometry/vertex_geometry.cpp",
@@ -50,24 +55,7 @@ int main() {
          "glm::vec2 &p1, const glm::vec2 &p2, float thickness) "},
         meta_utils::FilterMode::Whitelist);
 
-    std::cout << "Enter function invocation strings (type 'quit' to exit):\n";
+    start_interactive_invoker(extended_concrete_types);
 
-    std::string input;
-    while (true) {
-        std::cout << "> ";
-        std::getline(std::cin, input);
-
-        if (input == "quit")
-            break;
-
-        std::optional<std::string> result = invoke(input, extended_concrete_types);
-        if (result.has_value()) {
-            std::cout << "Result: " << result.value() << "\n";
-        } else {
-            std::cout << "Invocation failed.\n";
-        }
-    }
-
-    std::cout << "Goodbye!\n";
     return 0;
 }
